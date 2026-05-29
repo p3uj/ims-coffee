12bed1b96a2b1f330a1da4c290d2d2f64766c23d
BengieVillesco <bvillesco@gmail.com>
2026-05-29 21:55:40 +0800

    feat(app-module): integrate DatabaseModule and RoleModule into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
