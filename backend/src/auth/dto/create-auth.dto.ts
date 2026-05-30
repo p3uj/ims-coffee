c29185a5d835568526222205124f2ff382df9ff6
BengieVillesco <bvillesco@gmail.com>
2026-05-31 01:21:07 +0800

    feat(auth/dto): implement validation rules

--- backend/src/auth/dto/create-auth.dto.ts
+++ backend/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAuthDto {}
+import { IsEmail, IsInt, IsNotEmpty, Min } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsNotEmpty()
+  firstName!: string;
+
+  @IsNotEmpty()
+  lastName!: string;
+
+  @IsNotEmpty()
+  password!: string;
+
+  @IsInt()
+  @Min(1)
+  roleId!: number;
+}
