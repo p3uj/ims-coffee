8bab10a0a8133866137a5f2f484af488b09a6a08
BengieVillesco <bvillesco@gmail.com>
2026-05-31 18:21:50 +0800

    feat(auth/dto): add user query dto

---
+++ backend/src/auth/dto/user-query.dto.ts
@@ -0,0 +1,13 @@
+import { Transform } from 'class-transformer';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
+export class UserQueryDto {
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsOptional()
+  @Transform(({ value }) => value === 'true')
+  @IsBoolean()
+  isActive?: boolean;
+}
