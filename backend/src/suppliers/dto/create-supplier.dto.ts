a7450275ef3619a4efbe475509d332db94a9320a
BengieVillesco <bvillesco@gmail.com>
2026-06-01 22:50:09 +0800

    feat(suppliers/dto): implement create supplier validation

--- backend/src/suppliers/dto/create-supplier.dto.ts
+++ backend/src/suppliers/dto/create-supplier.dto.ts
@@ -1,1 +1,23 @@
-export class CreateSupplierDto {}
+import { RecordStatus } from '@prisma/client';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateSupplierDto {
+  @IsNotEmpty()
+  name!: string;
+
+  @IsOptional()
+  contactPerson?: string;
+
+  @IsNotEmpty()
+  phoneNumber!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  address!: string;
+
+  @IsEnum(RecordStatus)
+  @IsOptional()
+  recordStatus?: RecordStatus;
+}
