9a81fdc3077ccec20a1d7a44162022a5f233ea8f
BengieVillesco <bvillesco@gmail.com>
2026-06-02 15:25:48 +0800

    feat(ingredients): implement create dto validation

--- backend/src/ingredients/dto/create-ingredient.dto.ts
+++ backend/src/ingredients/dto/create-ingredient.dto.ts
@@ -1,1 +1,32 @@
-export class CreateIngredientDto {}
+import { RecordStatus } from '@prisma/client';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateIngredientDto {
+  @IsNotEmpty()
+  @IsNumber()
+  measurementUnitId!: number;
+
+  @IsNotEmpty()
+  @Transform(
+    ({ value }) =>
+      value
+        .trim()
+        .toLowerCase()
+        .replace(/\b\w/g, (char) => char.toUpperCase()), // Converts string to Title Case by capitalizing the first letter of each word
+  ) // Normalize Name
+  name!: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  currentStock!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  reorderLevel!: number;
+
+  @IsOptional()
+  @IsEnum(RecordStatus)
+  @Transform(({ value }) => value.toUpperCase())
+  recordStatus?: RecordStatus;
+}
