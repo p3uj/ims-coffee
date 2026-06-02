6668a8a5d362855cd0688a15ec4e9ada7fad03c3
BengieVillesco <bvillesco@gmail.com>
2026-06-02 12:37:22 +0800

    feat(measurement-units): implement create measurement unit validation

--- backend/src/measurement-units/dto/create-measurement-unit.dto.ts
+++ backend/src/measurement-units/dto/create-measurement-unit.dto.ts
@@ -1,1 +1,11 @@
-export class CreateMeasurementUnitDto {}
+import { Transform } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateMeasurementUnitDto {
+  @IsNotEmpty()
+  name!: string;
+
+  @IsNotEmpty()
+  @Transform(({ value }) => value.toUpperCase())
+  code!: string;
+}
