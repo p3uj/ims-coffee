1c48ecb7adee07c21c70b183dfb1051e0c5bc457
BengieVillesco <bvillesco@gmail.com>
2026-06-02 15:29:15 +0800

    feat(ingredients): implement CRUD operations with soft delete approach

--- backend/src/ingredients/ingredients.service.ts
+++ backend/src/ingredients/ingredients.service.ts
@@ -1,26 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { ingredientSelect } from './queries/ingredient.select';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class IngredientsService {
-  create(createIngredientDto: CreateIngredientDto) {
-    return 'This action adds a new ingredient';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createIngredientDto: CreateIngredientDto) {
+    const exisitingMeasureUnit =
+      await this.databaseService.measurementUnit.findUnique({
+        where: {
+          id: createIngredientDto.measurementUnitId,
+        },
+      });
+    const existingIngredient = await this.databaseService.ingredient.findFirst({
+      where: {
+        name: createIngredientDto.name,
+        measurementUnitId: createIngredientDto.measurementUnitId,
+      },
+    });
+
+    if (!exisitingMeasureUnit)
+      throw new NotFoundException('Measurement unit not found');
+
+    if (existingIngredient)
+      throw new ConflictException(
+        'Ingredient with the same name and measurement unit already exists',
+      );
+
+    return this.databaseService.ingredient.create({
+      data: createIngredientDto,
+      select: ingredientSelect,
+    });
   }
 
-  findAll() {
-    return `This action returns all ingredients`;
+  async findAll() {
+    return this.databaseService.ingredient.findMany({
+      select: ingredientSelect,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ingredient`;
+  async findOne(id: number) {
+    const existingIngredient = await this.databaseService.ingredient.findUnique(
+      {
+        where: {
+          id,
+        },
+      },
+    );
+
+    if (!existingIngredient)
+      throw new NotFoundException('Ingredient not found');
+
+    return existingIngredient;
   }
 
-  update(id: number, updateIngredientDto: UpdateIngredientDto) {
-    return `This action updates a #${id} ingredient`;
-  }
+  async update(id: number, updateIngredientDto: UpdateIngredientDto) {
+    const existingIngredient = await this.databaseService.ingredient.findUnique(
+      {
+        where: {
+          id,
+        },
+      },
+    );
 
-  remove(id: number) {
-    return `This action removes a #${id} ingredient`;
+    if (!existingIngredient)
+      throw new NotFoundException('Ingredient not found');
+
+    return this.databaseService.ingredient.update({
+      where: {
+        id,
+      },
+      data: updateIngredientDto,
+    });
   }
 }
