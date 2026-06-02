3db4f66792441bfd406366d48f1b0e320a788fd3
BengieVillesco <bvillesco@gmail.com>
2026-06-02 15:26:36 +0800

    feat(ingredients): add DatabaseModule dependency to module

--- backend/src/ingredients/ingredients.module.ts
+++ backend/src/ingredients/ingredients.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { IngredientsController } from './ingredients.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [IngredientsController],
   providers: [IngredientsService],
 })
