f3bc609c288b49575449a2c7e0fc5f7ebb6b04f8
BengieVillesco <bvillesco@gmail.com>
2026-06-02 12:40:35 +0800

    feat(measurement-units): add DatabaseModule dependency to MeasurementUnitsModule

--- backend/src/measurement-units/measurement-units.module.ts
+++ backend/src/measurement-units/measurement-units.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MeasurementUnitsService } from './measurement-units.service';
 import { MeasurementUnitsController } from './measurement-units.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [MeasurementUnitsController],
   providers: [MeasurementUnitsService],
 })
