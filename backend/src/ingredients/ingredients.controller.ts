e3d79b0b177e58e8ca21099f49a944f684b24056
BengieVillesco <bvillesco@gmail.com>
2026-06-02 15:28:48 +0800

    refactor(ingredients): use ParseIntPipe for route parameter validation

--- backend/src/ingredients/ingredients.controller.ts
+++ backend/src/ingredients/ingredients.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
@@ -18,17 +26,15 @@ export class IngredientsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ingredientsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ingredientsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateIngredientDto: UpdateIngredientDto) {
-    return this.ingredientsService.update(+id, updateIngredientDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ingredientsService.remove(+id);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateIngredientDto: UpdateIngredientDto,
+  ) {
+    return this.ingredientsService.update(id, updateIngredientDto);
   }
 }
