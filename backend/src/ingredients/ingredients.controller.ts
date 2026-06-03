1cf190399ee2a3d16e179f213f99bab7a1df9467
BengieVillesco <bvillesco@gmail.com>
2026-06-03 22:04:05 +0800

    feat(ingredients): implement query filtering

--- backend/src/ingredients/ingredients.controller.ts
+++ backend/src/ingredients/ingredients.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { IngredientQueryDto } from './dto/ingredient-query.dto';
 
 @Controller('ingredients')
 export class IngredientsController {
@@ -21,8 +23,8 @@ export class IngredientsController {
   }
 
   @Get()
-  findAll() {
-    return this.ingredientsService.findAll();
+  findAll(@Query() query: IngredientQueryDto) {
+    return this.ingredientsService.findAll(query);
   }
 
   @Get(':id')
