805519e581192906c16d57d0d8f277e26a290918
BengieVillesco <bvillesco@gmail.com>
2026-06-05 00:14:32 +0800

    feat(purchase-orders): implement validation rules

--- backend/src/purchase-orders/dto/create-purchase-order.dto.ts
+++ backend/src/purchase-orders/dto/create-purchase-order.dto.ts
@@ -1,1 +1,37 @@
-export class CreatePurchaseOrderDto {}
+import { PurchaseOrderStatus, RecordStatus } from '@prisma/client';
+import { Transform } from 'class-transformer';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreatePurchaseOrderDto {
+  @IsNotEmpty()
+  @IsNumber()
+  supplierId!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  createdBy!: number;
+
+  @IsNotEmpty()
+  @IsEnum(PurchaseOrderStatus)
+  @Transform(({ value }) => value.toUpperCase())
+  status!: PurchaseOrderStatus;
+
+  @IsNotEmpty()
+  @IsString()
+  orderDate!: string;
+
+  @IsOptional()
+  @IsString()
+  receivedDate?: string;
+
+  @IsOptional()
+  @IsEnum(RecordStatus)
+  @Transform(({ value }) => value.toUpperCase())
+  recordStatus?: RecordStatus;
+}
