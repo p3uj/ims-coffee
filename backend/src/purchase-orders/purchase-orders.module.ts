5fad6dee55ebcb4985a23701dae6843c3235d473
BengieVillesco <bvillesco@gmail.com>
2026-06-05 00:12:06 +0800

    feat(purchase-orders): add DatabaseModule dependency to module

--- backend/src/purchase-orders/purchase-orders.module.ts
+++ backend/src/purchase-orders/purchase-orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PurchaseOrdersService } from './purchase-orders.service';
 import { PurchaseOrdersController } from './purchase-orders.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchaseOrdersController],
   providers: [PurchaseOrdersService],
 })
