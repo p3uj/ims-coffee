4d5f149114425a0e52532aabaea9e8a43caafd9f
BengieVillesco <bvillesco@gmail.com>
2026-06-05 22:42:17 +0800

    feat(inventory-transactions): implement validation rules

--- backend/src/inventory-transactions/dto/create-inventory-transaction.dto.ts
+++ backend/src/inventory-transactions/dto/create-inventory-transaction.dto.ts
@@ -1,1 +1,36 @@
-export class CreateInventoryTransactionDto {}
+import { ReferenceType, TransactionType } from '@prisma/client';
+import { Transform, Type } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateInventoryTransactionDto {
+  @IsNotEmpty()
+  @IsNumber()
+  ingredientId!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  createdBy!: number;
+
+  @IsNotEmpty()
+  @Transform(({ value }) => value.toUpperCase())
+  @IsEnum(TransactionType)
+  transactionType!: TransactionType;
+
+  @IsNotEmpty()
+  @Transform(({ value }) => value.toUpperCase())
+  @IsEnum(ReferenceType)
+  referenceType!: ReferenceType;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  quantity!: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  referenceId?: number;
+
+  @IsOptional()
+  notes?: string;
+}
