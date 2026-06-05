17b7298c554b3b592888836e112204c5fdeb203c
BengieVillesco <bvillesco@gmail.com>
2026-06-05 12:26:20 +0800

    feat(purchase-order-items): implement CRUD operations with soft delete approach

--- backend/src/purchase-order-items/purchase-order-items.service.ts
+++ backend/src/purchase-order-items/purchase-order-items.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePurchaseOrderItemDto } from './dto/create-purchase-order-item.dto';
 import { UpdatePurchaseOrderItemDto } from './dto/update-purchase-order-item.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { PurchaseOrderItemQueryDto } from './dto/purchase-order-item-query.dto';
+import { purchaseOrderItemWhere } from './queries/purchase-order-item.where';
+import { purchaseOrderItemSelect } from './queries/purchase-order-item.select';
 
 @Injectable()
 export class PurchaseOrderItemsService {
-  create(createPurchaseOrderItemDto: CreatePurchaseOrderItemDto) {
-    return 'This action adds a new purchaseOrderItem';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createPurchaseOrderItemDto: CreatePurchaseOrderItemDto) {
+    return this.databaseService.purchaseOrderItem.create({
+      data: createPurchaseOrderItemDto,
+      select: purchaseOrderItemSelect,
+    });
   }
 
-  findAll() {
-    return `This action returns all purchaseOrderItems`;
+  async findAll(query: PurchaseOrderItemQueryDto) {
+    return this.databaseService.purchaseOrderItem.findMany({
+      where: purchaseOrderItemWhere(query),
+      select: purchaseOrderItemSelect,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} purchaseOrderItem`;
+  async findOne(id: number) {
+    const existingPurchaseOrderItem =
+      await this.databaseService.purchaseOrderItem.findUnique({
+        where: {
+          id,
+        },
+        select: purchaseOrderItemSelect,
+      });
+
+    if (!existingPurchaseOrderItem)
+      throw new NotFoundException('Purchase order item not found');
+
+    return existingPurchaseOrderItem;
   }
 
-  update(id: number, updatePurchaseOrderItemDto: UpdatePurchaseOrderItemDto) {
-    return `This action updates a #${id} purchaseOrderItem`;
-  }
+  async update(
+    id: number,
+    updatePurchaseOrderItemDto: UpdatePurchaseOrderItemDto,
+  ) {
+    const existingPurchaseOrderItem =
+      await this.databaseService.purchaseOrderItem.findUnique({
+        where: {
+          id,
+        },
+      });
 
-  remove(id: number) {
-    return `This action removes a #${id} purchaseOrderItem`;
+    if (!existingPurchaseOrderItem)
+      throw new NotFoundException('Purchase order item not found');
+
+    return this.databaseService.purchaseOrderItem.update({
+      where: {
+        id,
+      },
+      data: updatePurchaseOrderItemDto,
+      select: purchaseOrderItemSelect,
+    });
   }
 }
