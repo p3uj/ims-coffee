c81d931c2fec1d8cac0816eb2f9d91d58326dfc0
BengieVillesco <bvillesco@gmail.com>
2026-06-05 22:41:47 +0800

    feat(inventory-transactions): add DatabaseModule dependency to module

--- backend/src/inventory-transactions/inventory-transactions.module.ts
+++ backend/src/inventory-transactions/inventory-transactions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InventoryTransactionsService } from './inventory-transactions.service';
 import { InventoryTransactionsController } from './inventory-transactions.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [InventoryTransactionsController],
   providers: [InventoryTransactionsService],
 })
