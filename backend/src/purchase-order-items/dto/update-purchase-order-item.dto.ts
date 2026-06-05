f4e44d2fab489bf74e95dcbd90d22251307e83fe
BengieVillesco <bvillesco@gmail.com>
2026-06-05 12:24:36 +0800

    feat(purchase-order-items): support record status updates

--- backend/src/purchase-order-items/dto/update-purchase-order-item.dto.ts
+++ backend/src/purchase-order-items/dto/update-purchase-order-item.dto.ts
@@ -1,6 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePurchaseOrderItemDto } from './create-purchase-order-item.dto';
+import { RecordStatus } from '@prisma/client';
+import { IsEnum, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UpdatePurchaseOrderItemDto extends PartialType(
   CreatePurchaseOrderItemDto,
-) {}
+) {
+  @IsOptional()
+  @Transform(({ value }) => value.toUpperCase())
+  @IsEnum(RecordStatus)
+  recordStatus?: RecordStatus;
+}
