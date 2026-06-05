2dea059d2821cfedd77d15b38fe8d6712fec50b8
BengieVillesco <bvillesco@gmail.com>
2026-06-05 22:42:43 +0800

    feat(inventory-transactions): support record status updates

--- backend/src/inventory-transactions/dto/update-inventory-transaction.dto.ts
+++ backend/src/inventory-transactions/dto/update-inventory-transaction.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateInventoryTransactionDto } from './create-inventory-transaction.dto';
+import { RecordStatus } from '@prisma/client';
+import { IsEnum, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateInventoryTransactionDto extends PartialType(CreateInventoryTransactionDto) {}
+export class UpdateInventoryTransactionDto extends PartialType(
+  CreateInventoryTransactionDto,
+) {
+  @IsOptional()
+  @Transform(({ value }) => value.toUpperCase())
+  @IsEnum(RecordStatus)
+  recordStatus?: RecordStatus;
+}
