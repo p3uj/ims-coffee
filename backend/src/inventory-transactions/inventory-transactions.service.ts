209ee00f977c1f6f0b649a4fc63b524945fac3d6
BengieVillesco <bvillesco@gmail.com>
2026-06-05 22:45:39 +0800

    feat(inventory-transactions): implement CRUD operations with soft delete approach

--- backend/src/inventory-transactions/inventory-transactions.service.ts
+++ backend/src/inventory-transactions/inventory-transactions.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateInventoryTransactionDto } from './dto/create-inventory-transaction.dto';
 import { UpdateInventoryTransactionDto } from './dto/update-inventory-transaction.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { InventoryTransactionQueryDto } from './dto/inventory-transaction-query.dto';
+import { inventoryTransactionWhere } from './queries/inventory-transaction.where';
+import { inventoryTransactionSelect } from './queries/inventory-transaction.select';
 
 @Injectable()
 export class InventoryTransactionsService {
-  create(createInventoryTransactionDto: CreateInventoryTransactionDto) {
-    return 'This action adds a new inventoryTransaction';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createInventoryTransactionDto: CreateInventoryTransactionDto) {
+    return this.databaseService.inventoryTransaction.create({
+      data: createInventoryTransactionDto,
+      select: inventoryTransactionSelect,
+    });
   }
 
-  findAll() {
-    return `This action returns all inventoryTransactions`;
+  async findAll(query: InventoryTransactionQueryDto) {
+    return this.databaseService.inventoryTransaction.findMany({
+      where: inventoryTransactionWhere(query),
+      select: inventoryTransactionSelect,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} inventoryTransaction`;
+  async findOne(id: number) {
+    const existingInventoryTrans =
+      await this.databaseService.inventoryTransaction.findUnique({
+        where: {
+          id,
+        },
+        select: inventoryTransactionSelect,
+      });
+
+    if (!existingInventoryTrans)
+      throw new NotFoundException('Inventory transaction not found');
+
+    return existingInventoryTrans;
   }
 
-  update(id: number, updateInventoryTransactionDto: UpdateInventoryTransactionDto) {
-    return `This action updates a #${id} inventoryTransaction`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} inventoryTransaction`;
+  async update(
+    id: number,
+    updateInventoryTransactionDto: UpdateInventoryTransactionDto,
+  ) {
+    return this.databaseService.inventoryTransaction.update({
+      where: {
+        id,
+      },
+      data: updateInventoryTransactionDto,
+      select: inventoryTransactionSelect,
+    });
   }
 }
