69d4aec1b7cfa6c4d82040001aeb92dd5e2ba1e1
BengieVillesco <bvillesco@gmail.com>
2026-06-05 12:23:27 +0800

    feat(purchase-order-items): implement validation rules

--- backend/src/purchase-order-items/dto/create-purchase-order-item.dto.ts
+++ backend/src/purchase-order-items/dto/create-purchase-order-item.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePurchaseOrderItemDto {}
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreatePurchaseOrderItemDto {
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  purchaseOrderId!: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  ingredientId!: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  quantity!: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  unitCost!: number;
+}
