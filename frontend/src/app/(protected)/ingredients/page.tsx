ed4e45abdbd04bdda08831127a125eced5351a7b
BengieVillesco <bvillesco@gmail.com>
2026-06-10 23:56:13 +0800

    feat(ingredients): initialize ingredients page

---
+++ frontend/src/app/(protected)/ingredients/page.tsx
@@ -0,0 +1,12 @@
+import { IngredientsTable } from "@/components/ingredients/ingredients-table";
+
+export default function Ingredients() {
+  return (
+    <>
+      <header>Ingredients</header>
+      <main>
+        <IngredientsTable />
+      </main>
+    </>
+  );
+}
