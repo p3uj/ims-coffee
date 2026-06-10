ce0d401474a2bde652afe6645a6cdc1d3afe3b05
BengieVillesco <bvillesco@gmail.com>
2026-06-10 23:55:13 +0800

    feat(components): add ingredients columns

---
+++ frontend/src/components/ingredients/ingredients-columns.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "../ui/button";
+import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react";
+import { IngredientsDialog } from "./ingredients-dialog";
+
+export const ingredientsColumns: ColumnDef<Ingredient>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Email
+        {column.getIsSorted() === "desc" ? (
+          <span className="flex">
+            <ArrowDown className="ml-2 h-4 w-4" />
+          </span>
+        ) : column.getIsSorted() === "asc" ? (
+          <ArrowUp className="ml-2 h-4 w-4" />
+        ) : (
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+    ),
+  },
+  {
+    accessorKey: "currentStock",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Current Stock
+        {column.getIsSorted() === "desc" ? (
+          <span className="flex">
+            <ArrowDown className="ml-2 h-4 w-4" />
+          </span>
+        ) : column.getIsSorted() === "asc" ? (
+          <ArrowUp className="ml-2 h-4 w-4" />
+        ) : (
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+    ),
+  },
+  {
+    accessorKey: "reorderLevel",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Reorder Level
+        {column.getIsSorted() === "desc" ? (
+          <span className="flex">
+            <ArrowDown className="ml-2 h-4 w-4" />
+          </span>
+        ) : column.getIsSorted() === "asc" ? (
+          <ArrowUp className="ml-2 h-4 w-4" />
+        ) : (
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+    ),
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const ingredient = row.original;
+
+      return <IngredientsDialog />;
+    },
+  },
+];
