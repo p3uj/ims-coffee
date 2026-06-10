b4b1cca92fe2355ad2f1bac9857012eb8f12f694
BengieVillesco <bvillesco@gmail.com>
2026-06-10 23:53:36 +0800

    feat(mock): add ingredients mock data

---
+++ frontend/src/lib/mock/mock-ingredients.ts
@@ -0,0 +1,14 @@
+export const mockIngredients: Ingredient[] = [
+  {
+    id: 1,
+    name: "Ingredient 1",
+    currentStock: 20,
+    reorderLevel: 5,
+  },
+  {
+    id: 2,
+    name: "Ingredient 2",
+    currentStock: 15,
+    reorderLevel: 7,
+  },
+];
