c11d0c1be0fb9a5a1c68a5619e77ebb12c470342
BengieVillesco <bvillesco@gmail.com>
2026-06-12 03:26:12 +0800

    feat(ingredients): allow searchable fields on table

--- frontend/src/components/ingredients/ingredients-table.tsx
+++ frontend/src/components/ingredients/ingredients-table.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import { mockIngredients } from "@/lib/mock/mock-ingredients";
 import { DataTable } from "../ui/data-table";
 import { ingredientsColumns } from "./ingredients-columns";
 
 export function IngredientsTable() {
+  const searchableFields = ["name", "currentStock", "reorderLevel"];
+
   return (
     <div className="hidden lg:block">
-      <DataTable columns={ingredientsColumns} data={mockIngredients} />
+      <DataTable
+        columns={ingredientsColumns}
+        data={mockIngredients}
+        searchableFields={searchableFields}
+      />
     </div>
   );
 }
