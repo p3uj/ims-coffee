fb5a9979f5bb582d4f4a9853877770540ba93d8b
BengieVillesco <bvillesco@gmail.com>
2026-06-14 16:56:37 +0800

    chore(mock): update mock ingredients to include status field

--- frontend/src/lib/mock/mock-ingredients.ts
+++ frontend/src/lib/mock/mock-ingredients.ts
@@ -1,3 +1,6 @@
+import { INGREDIENT_STATUS } from "@/constants/ingredients";
+import { Ingredient } from "@/types/ingredients";
+
 export const mockIngredients: Ingredient[] = [
   {
     id: 1,
@@ -5,12 +8,14 @@ export const mockIngredients: Ingredient[] = [
     initialStock: 30,
     currentStock: 20,
     reorderLevel: 5,
+    status: INGREDIENT_STATUS.IN_STOCK,
   },
   {
     id: 2,
     name: "Ingredient 2",
     initialStock: 20,
-    currentStock: 15,
+    currentStock: 5,
     reorderLevel: 7,
+    status: INGREDIENT_STATUS.LOW_STOCK,
   },
 ];
