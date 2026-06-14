4f1786571778e9b994075a8dbd2554896597363b
BengieVillesco <bvillesco@gmail.com>
2026-06-14 21:15:25 +0800

    chore(mock): update mock ingredients to include category field

--- frontend/src/lib/mock/mock-ingredients.ts
+++ frontend/src/lib/mock/mock-ingredients.ts
@@ -5,6 +5,7 @@ export const mockIngredients: Ingredient[] = [
   {
     id: 1,
     name: "Ingredient 1",
+    category: "Category 1",
     initialStock: 30,
     currentStock: 20,
     reorderLevel: 5,
@@ -13,6 +14,7 @@ export const mockIngredients: Ingredient[] = [
   {
     id: 2,
     name: "Ingredient 2",
+    category: "Category 2",
     initialStock: 20,
     currentStock: 5,
     reorderLevel: 7,
