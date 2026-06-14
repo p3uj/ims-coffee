53084468fa3cb422838a5d127537735576e3888e
BengieVillesco <bvillesco@gmail.com>
2026-06-14 12:36:11 +0800

    feat(mock): add initialStock on ingredients mock data

--- frontend/src/lib/mock/mock-ingredients.ts
+++ frontend/src/lib/mock/mock-ingredients.ts
@@ -2,12 +2,14 @@ export const mockIngredients: Ingredient[] = [
   {
     id: 1,
     name: "Ingredient 1",
+    initialStock: 30,
     currentStock: 20,
     reorderLevel: 5,
   },
   {
     id: 2,
     name: "Ingredient 2",
+    initialStock: 20,
     currentStock: 15,
     reorderLevel: 7,
   },
