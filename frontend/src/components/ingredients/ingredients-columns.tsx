ae0321e8e0659e3b356cd241b958dde7756d8290
BengieVillesco <bvillesco@gmail.com>
2026-06-14 16:57:51 +0800

    feat(ingredients): add status column and improve stock color logic

--- frontend/src/components/ingredients/ingredients-columns.tsx
+++ frontend/src/components/ingredients/ingredients-columns.tsx
@@ -7,6 +7,7 @@ import { IngredientsDialog } from "./ingredients-dialog";
 import { Progress } from "../ui/progress";
 import { getBgColor } from "@/lib/utils/color";
 import { Badge } from "../ui/badge";
+import { Ingredient } from "@/types/ingredients";
 
 export const ingredientsColumns: ColumnDef<Ingredient>[] = [
   {
@@ -49,15 +50,15 @@ export const ingredientsColumns: ColumnDef<Ingredient>[] = [
     cell: ({ row }) => {
       const currentStock = row.original.currentStock;
       const initialStock = row.original.initialStock;
-      const bgColor = getBgColor(currentStock, row.original.reorderLevel);
+      const bgColor = getBgColor(row.original.status);
 
       return (
         <div className="flex items-center text-gray-500">
           <Progress
             value={(currentStock / initialStock) * 100}
-            className={`h-[8px] [&>div]:bg-${bgColor} mr-2 max-w-[150px]`}
+            className={`h-[8px] [&>div]:bg-${bgColor.color} mr-2 max-w-[150px]`}
           />
-          <span className={`text-${bgColor}`}>{currentStock}</span> /{" "}
+          <span className={`text-${bgColor.color}`}>{currentStock}</span>/
           {initialStock}
         </div>
       );
@@ -84,6 +85,35 @@ export const ingredientsColumns: ColumnDef<Ingredient>[] = [
     cell: ({ row }) => (
       <Badge className="px-1.5">{row.original.reorderLevel}</Badge>
     ),
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Status
+        {column.getIsSorted() === "desc" ? (
+          <span className="flex">
+            <ArrowDown className="ml-2 h-4 w-4" />
+          </span>
+        ) : column.getIsSorted() === "asc" ? (
+          <ArrowUp className="ml-2 h-4 w-4" />
+        ) : (
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const bgColor = getBgColor(row.original.status);
+
+      return (
+        <Badge
+          className={`px-1.5 bg-${bgColor.color100} text-${bgColor.color} before:h-1.5 before:w-1.5 before:bg-${bgColor.color} before:rounded-full`}>
+          {row.original.status}
+        </Badge>
+      );
+    },
   },
   {
     id: "actions",
