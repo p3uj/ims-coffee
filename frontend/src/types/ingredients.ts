8d64d5fc0b8c199d97f2e2de8eee3cedf3c20e70
BengieVillesco <bvillesco@gmail.com>
2026-06-14 22:35:40 +0800

    feat(types): introduce IngredientCategory type literal and change ingredient category property from string to strict type literal

--- frontend/src/types/ingredients.ts
+++ frontend/src/types/ingredients.ts
@@ -1,12 +1,18 @@
-import { INGREDIENT_STATUS } from "@/constants/ingredients";
+import {
+  INGREDIENT_CATEGORIES,
+  INGREDIENT_STATUS,
+} from "@/constants/ingredients";
 
 export type IngredientStatus =
   (typeof INGREDIENT_STATUS)[keyof typeof INGREDIENT_STATUS];
 
+export type IngredientCategory =
+  (typeof INGREDIENT_CATEGORIES)[keyof typeof INGREDIENT_CATEGORIES];
+
 export interface Ingredient {
   id: number;
   name: string;
-  category: string;
+  category: IngredientCategory;
   initialStock: number;
   currentStock: number;
   reorderLevel: number;
