82007506482574f4ff53c5a406c02a9bcd6a7685
BengieVillesco <bvillesco@gmail.com>
2026-06-14 21:12:14 +0800

    fix(utils): fix dynamic tailwind styling issue in getBgColor

--- frontend/src/lib/utils/color.tsx
+++ frontend/src/lib/utils/color.tsx
@@ -3,20 +3,26 @@ import { IngredientStatus } from "@/types/ingredients";
 export function getBgColor(status: IngredientStatus) {
   if (status === "In Stock") {
     return {
-      color: "green-500",
-      color100: "green-100",
+      text: "text-green-500",
+      bg: "bg-green-100",
+      indicator: "before:bg-green-500",
+      progressDiv: "[&>div]:bg-green-500",
     };
   }
 
   if (status === "Low Stock") {
     return {
-      color: "amber-500",
-      color100: "amber-100",
+      text: "text-amber-500",
+      bg: "bg-amber-100",
+      indicator: "before:bg-amber-500",
+      progressDiv: "[&>div]:bg-amber-500",
     };
   }
 
   return {
-    color: "red-500",
-    color100: "red-100",
+    text: "text-red-500",
+    bg: "bg-red-100",
+    indicator: "before:bg-red-500",
+    progressDiv: "[&>div]:bg-red-500",
   };
 }
